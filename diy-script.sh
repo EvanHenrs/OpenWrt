a1d7453eff10245882b11975e505502b9aa8243e
EvanHenrs <103309288+EvanHenrs@users.noreply.github.com>
2023-09-23 09:07:57 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -27,6 +27,7 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-netdata
 rm -rf feeds/luci/applications/luci-app-serverchan
 rm -rf feeds/luci/applications/luci-app-upnp
+rm -rf feeds/luci/applications/luci-app-vlmcsd
 
 # 添加额外插件
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
